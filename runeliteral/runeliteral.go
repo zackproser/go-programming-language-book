021ce493ee3eda5c87fec7878c19bd45f73837a6
Zack Proser <zackproser@gmail.com>
2020-05-10 12:11:58 -0700

    Add hexidecimal and rune literal examples

---
+++ runeliteral/runeliteral.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	ascii := 'a'
+	unicode := 'か'
+	newline := '\n'
+	fmt.Printf("%d %[1]c %[1]q\n", ascii)
+	fmt.Printf("%d %[1]c %[1]q\n", unicode)
+	fmt.Printf("%d %[1]q\n", newline)
+}
