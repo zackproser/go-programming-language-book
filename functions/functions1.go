d04f350ac900fa915f78108d2858ca9fba6ab6e2
Zack Proser <zackproser@gmail.com>
2020-06-08 19:32:47 -0700

    Add functions examples

---
+++ functions/functions1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func square(n int) int     { return n * n }
+func negative(n int) int   { return -n }
+func product(m, n int) int { return m * n }
+
+func main() {
+	f := square
+	fmt.Println(f(3))
+
+	f = negative
+	fmt.Println(f(3))
+	fmt.Printf("%T\n", f)
+}
